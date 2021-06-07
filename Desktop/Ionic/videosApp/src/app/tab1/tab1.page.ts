b563388370fd5825b9fd3176cd45dd88c3bf2a26
Marcos Antonio Estremote <marcos.estremote@etec.sp.gov.br>
2021-06-06 23:43:51 -0300

    Adicionamos array de filmes e a interface de filmes

--- Desktop/Ionic/videosApp/src/app/tab1/tab1.page.ts
+++ Desktop/Ionic/videosApp/src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/IFilme.models';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,35 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+
+  titulo = "Vídeos App";
+  listaVideos: IFilme[] = [
+    {
+        nome:'The Flash (2014)',
+        lancamento:'06/06/2021',
+        duracao: '44m',
+        classificacao:76,
+        cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg',
+        generos: ['Drama', 'Sci-Fi', 'Fantasy']
+    },
+    {
+      nome:'Tom & Jerry: O Filme (2021)',
+      lancamento:'11/02/2021 (BR)',
+      duracao: '1h 41m',
+      classificacao:73,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9NvYyM8H6d5KAVGqpyFV9YPO5cU.jpg',
+      generos: ['Comédia', 'Família', 'Animação']
+    },
+    {
+      nome:'Guarda Costas',
+      lancamento:'11/02/1998 (BR)',
+      duracao: '1h 28m',
+      classificacao:70,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9NvYyM8H6d5KAVGqpyFV9YPO5cU.jpg',
+      generos: ['Comédia', 'Família', 'Animação']
+    }
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
